1436f69f7e51c2514652ed6972b39f34d8876762
LorenzoTL <lorenzot7@gmail.com>
2015-07-20 15:10:16 +0200

    Funciona Buscar Problemas, Funciona lista problemas

--- aceptaElReto/src/acr/estructuras/CategoryWSType.java
+++ aceptaElReto/src/acr/estructuras/CategoryWSType.java
@@ -7,10 +7,10 @@ import org.simpleframework.xml.Root;
 
 /**
  * Clase usada por los WS cuando tienen que responder con
- * informaciÃ³n de una categorÃ­a.
+ * información de una categoría.
  * 
- * Tiene todos los atributos imaginables sobre categorÃ­as,
- * aunque dependiendo del WS concreto, se rellenarÃ¡n mÃ¡s
+ * Tiene todos los atributos imaginables sobre categorías,
+ * aunque dependiendo del WS concreto, se rellenarán más
  * o menos.
  *
  * @author Marco Antonio Gomez Martin
@@ -19,9 +19,9 @@ import org.simpleframework.xml.Root;
 public class CategoryWSType {
 	
 	/**
-	 * VacÃ­a todos los campos (los pone a null), excepto
-	 * las subcategorÃ­as. Ãštil para WS que utilizan una
-	 * categorÃ­a dummy para devolver una lista de categorÃ­as.
+	 * Vacía todos los campos (los pone a null), excepto
+	 * las subcategorías. Útil para WS que utilizan una
+	 * categoría dummy para devolver una lista de categorías.
 	 */
 	public void vaciaInfo() {
 		this.id = null;
@@ -34,106 +34,285 @@ public class CategoryWSType {
 	}
 	
 	
-	/** Identificador de la categorÃ­a */
+	/** Identificador de la categoría */
 	public Integer id;
 	
-	/** Nombre de la categorÃ­a */
+	/** Nombre de la categoría */
 	public String name;
 	
-	/** DescripciÃ³n de la categorÃ­a */
+	/** Descripción de la categoría */
 	public String desc;
 	
-	/** Lista de categorÃ­as hijas */
-	public List<CategoryWSType> subcats;
+	/** Lista de categorías hijas */
+	public List<CategoryWSType> subcats_lista;
+	//public CategoryWSType[] subcats;
 
-	/** Lista de problemas directamente pertenecientes a la categorÃ­a. */
+	public List<CategoryWSType> getSubcats() {
+		return subcats_lista;
+	}
+
+	public void setSubcats(List<CategoryWSType> subcats) {
+		this.subcats_lista = subcats;
+	}
+
+	public List<CategoryWSType> getPath() {
+		return path_lista;
+	}
+
+	public void setPath(List<CategoryWSType> path) {
+		this.path_lista = path;
+	}
+
+
+	/** Lista de problemas directamente pertenecientes a la categoría. */
 	@Element(name="problems", required = false)
 	public ProblemDetailsList problems;
 	
 	/** 
-	 * Ruta desde la raÃ­z del Ã¡rbol hasta esta categorÃ­a.
-	 * El primer elemento es la categorÃ­a raÃ­z. El Ãºltimo es la
-	 * categorÃ­a PADRE al objeto. Contienen Ãºnicamente id y name.
+	 * Ruta desde la raíz del árbol hasta esta categoría.
+	 * El primer elemento es la categoría raíz. El último es la
+	 * categoría PADRE al objeto. Contienen únicamente id y name.
 	 */
 	@Element(name="path", required = false)
-	public List<CategoryWSType> path;
-	
+	public List<CategoryWSType> path_lista;
+	//public CategoryWSType[] path;
 
 	// ////
-	// EstadÃ­sticas. Son mantenidas por un proceso en segundo plano.
+	// Estadísticas. Son mantenidas por un proceso en segundo plano.
 	// Por tanto, deben verse como un dato "volatil" y que puede
-	// no estar actualizado. Incluso el nÃºmero de problemas puede
-	// ser incorrecto, si se aÃ±adieron mÃ¡s problemas tras la
-	// Ãºltima actualizaciÃ³n de las estadÃ­sticas.
+	// no estar actualizado. Incluso el número de problemas puede
+	// ser incorrecto, si se añadieron más problemas tras la
+	// última actualización de las estadísticas.
 	// ////
 	
 	// ////
-	// EstadÃ­sticas de la categorÃ­a (sin incluir subcategorÃ­as)
+	// Estadísticas de la categoría (sin incluir subcategorías)
 	// ////
 	
-	/** NÃºmero de problemas pertenecientes directamente a
-	la categorÃ­a */
+	/** Número de problemas pertenecientes directamente a
+	la categoría */
 	public Integer numOfProblems;
 	
 	// ////
-	// EstadÃ­sticas de la categorÃ­a y subcategorÃ­as
+	// Estadísticas de la categoría y subcategorías
 	// ////
 	
-	/** NÃºmero de problemas en la categorÃ­a y subcategorÃ­as. */
+	/** Número de problemas en la categoría y subcategorías. */
 	public Integer numOfAllProblems;
 
 	// ///
-	// EstadÃ­sticas de veredictos
+	// Estadísticas de veredictos
 	// ///
 	
-	/** EnvÃ­os totales del problema */
+	/** Envíos totales del problema */
 	public Integer totalSubs;
 
-	/** NÃºmero de envÃ­os aceptados */
+	/** Número de envíos aceptados */
 	public Integer ac;
 
-	/** NÃºmero de envÃ­os con PE */
+	/** Número de envíos con PE */
 	public Integer pe;
 
-	/** NÃºmero de envÃ­os con WA */
+	/** Número de envíos con WA */
 	public Integer wa;
 
-	/** NÃºmero de envÃ­os con TL */
+	/** Número de envíos con TL */
 	public Integer tl;
 
-	/** NÃºmero de envÃ­os con ML */
+	/** Número de envíos con ML */
 	public Integer ml;
 
-	/** NÃºmero de envÃ­os con OL */
+	/** Número de envíos con OL */
 	public Integer ol;
 
-	/** NÃºmero de envÃ­os con RF */
+	/** Número de envíos con RF */
 	public Integer rf;
 
-	/** NÃºmero de envÃ­os con RTE */
+	/** Número de envíos con RTE */
 	public Integer rte;
 
-	/** NÃºmero de envÃ­os con CE */
+	/** Número de envíos con CE */
 	public Integer ce;
 
-	/** NÃºmero de envÃ­os con IR */
+	/** Número de envíos con IR */
 	public Integer ir;
 
 	// ////
-	// EstadÃ­sticas de lenguajes
+	// Estadísticas de lenguajes
 	// ////
 	
-	/** NÃºmero de envÃ­os en C */
+	/** Número de envíos en C */
 	public Integer c;
 	
-	/** NÃºmero de envÃ­os en C++ */
+	/** Número de envíos en C++ */
 	public Integer cpp;
 	
-	/** NÃºmero de envÃ­os en Java */
+	/** Número de envíos en Java */
 	public Integer java;	
 
-	/** Constructor sin parÃ¡metros. No inicializa nada...
-	 * EstÃ¡ para que funcione el marshall/unmarshall.
+	/** Constructor sin parámetros. No inicializa nada...
+	 * Está para que funcione el marshall/unmarshall.
 	 */
 	public CategoryWSType() {}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public ProblemDetailsList getProblems() {
+		return problems;
+	}
+
+	public void setProblems(ProblemDetailsList problems) {
+		this.problems = problems;
+	}
+
+	public Integer getNumOfProblems() {
+		return numOfProblems;
+	}
+
+	public void setNumOfProblems(Integer numOfProblems) {
+		this.numOfProblems = numOfProblems;
+	}
+
+	public Integer getNumOfAllProblems() {
+		return numOfAllProblems;
+	}
+
+	public void setNumOfAllProblems(Integer numOfAllProblems) {
+		this.numOfAllProblems = numOfAllProblems;
+	}
+
+	public Integer getTotalSubs() {
+		return totalSubs;
+	}
+
+	public void setTotalSubs(Integer totalSubs) {
+		this.totalSubs = totalSubs;
+	}
+
+	public Integer getAc() {
+		return ac;
+	}
+
+	public void setAc(Integer ac) {
+		this.ac = ac;
+	}
+
+	public Integer getPe() {
+		return pe;
+	}
+
+	public void setPe(Integer pe) {
+		this.pe = pe;
+	}
+
+	public Integer getWa() {
+		return wa;
+	}
+
+	public void setWa(Integer wa) {
+		this.wa = wa;
+	}
+
+	public Integer getTl() {
+		return tl;
+	}
+
+	public void setTl(Integer tl) {
+		this.tl = tl;
+	}
+
+	public Integer getMl() {
+		return ml;
+	}
+
+	public void setMl(Integer ml) {
+		this.ml = ml;
+	}
+
+	public Integer getOl() {
+		return ol;
+	}
+
+	public void setOl(Integer ol) {
+		this.ol = ol;
+	}
+
+	public Integer getRf() {
+		return rf;
+	}
+
+	public void setRf(Integer rf) {
+		this.rf = rf;
+	}
+
+	public Integer getRte() {
+		return rte;
+	}
+
+	public void setRte(Integer rte) {
+		this.rte = rte;
+	}
+
+	public Integer getCe() {
+		return ce;
+	}
+
+	public void setCe(Integer ce) {
+		this.ce = ce;
+	}
+
+	public Integer getIr() {
+		return ir;
+	}
+
+	public void setIr(Integer ir) {
+		this.ir = ir;
+	}
+
+	public Integer getC() {
+		return c;
+	}
+
+	public void setC(Integer c) {
+		this.c = c;
+	}
+
+	public Integer getCpp() {
+		return cpp;
+	}
+
+	public void setCpp(Integer cpp) {
+		this.cpp = cpp;
+	}
+
+	public Integer getJava() {
+		return java;
+	}
+
+	public void setJava(Integer java) {
+		this.java = java;
+	}
+	
 }
